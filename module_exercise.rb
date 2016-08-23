16774a934cccca68129239c93ec7c0c0bac2b5ca
Emily Abrash <emabrash@gmail.com>
2016-08-23 10:24:24 -0700

    Class demo code

--- module_exercise.rb
+++ module_exercise.rb
@@ -2,21 +2,49 @@
 ######      NAMESPACE        #####
 ###################################
 
-# Provides a container to hold things like methods, classes and constants as a way to group them together logically and to help avoid conflicts with other methods and classes that may have the same name,
-#typically written by someone else (**Ahem, gems**).
+# Provides a container to hold things like methods, classes and constants as a way to group them together logically
+# and to help avoid conflicts with other methods and classes that may have the same name,
+# typically written by someone else (**Ahem, gems**).
 
 
 ###################################
 ######    EXAMPLE MODULE      #####
 ###################################
 
-  #
+
   # module Perimeter
+  #
+  #   AWESOME_PERIMETER = 37
+  #
   #   class Array
   #     def initialize
   #       @size = 400
   #     end
+  #
+  #     def length
+  #       return @size
+  #     end
+  #
   #   end
+  #
+  #   class Puppy
+  #
+  #     AGE_IT_CAN_GO_HOME = 8
+  #
+  #     attr_accessor :name, :breed, :age
+  #
+  #     def initialize (name, breed, age)
+  #       @name = name
+  #       @breed = breed
+  #       @age = age
+  #     end
+  #
+  #     def to_s
+  #       return "#{@name}, #{@breed}, #{@age}"
+  #     end
+  #
+  #   end
+  #
   # end
   #
   # our_array = Perimeter::Array.new
@@ -26,7 +54,14 @@
   # p ruby_array.class
   #
   # p our_array.length
-
+  #
+  # p Perimeter::AWESOME_PERIMETER
+  #
+  # bestpuppy = Perimeter::Puppy.new("Bosco", "terrier", "6")
+  #
+  # p bestpuppy.to_s
+  #
+  # p Perimeter::Puppy::AGE_IT_CAN_GO_HOME
 
 
 
@@ -56,16 +91,16 @@
 
 #Testing out the traveller gem!
 
-  # require 'traveller'
-  #
-  # trav = Traveller.new("Seattle, Minnesota 98101")
+  require 'traveller'
 
-  # puts traveller1.city
-  # puts traveller1.zip
-  #
-  # trav.state = "Washington"
-  #
-  # puts traveller1.state
+  traveller1 = Traveller.new("Seattle, Minnesota 98101")
+
+  puts traveller1.city
+  puts traveller1.zip
+
+  traveller1.state = "Washington"
+
+  puts traveller1.state
 
 
 ###################################
@@ -73,18 +108,30 @@
 ###################################
 
 #I want to create a new class for travellers, called traveller. What could possibly go wrong?
-  # class Traveller
-  #   attr_accessor :name, :email, :city, :state
-  #   def initialize(details_hash)
-  #     @name = details_hash[:name]
-  #     @email = details_hash[:email]
-  #     @location = Traveller.new(details_hash[:location])
-  #   end
-  # end
 
-  #Tests to use the Traveller Gem (after my class has been created. )
-    # traveller2 = Traveller.new("Chicago 60611 IL")
-    # puts Traveller.state
+# module Travel
+#   class Traveller
+#     attr_accessor :name, :email, :location
+#
+#     def initialize(details_hash)
+#       @name = details_hash[:name]
+#       @email = details_hash[:email]
+#       @location = details_hash[:location]
+#     end
+#
+#   end
+# end
+#
+# #Tests to use the Traveller Gem (after my class has been created. )
+#   traveller2 = Traveller.new("Chicago 60611 IL")
+#   puts traveller2.state
+#
+#   location = Traveller.new("Kirkland, WA 98033")
+#
+#   user1 = Travel::Traveller.new({name: "Satine the Cat", email: "satinemarine@kittymail.com", location: location})
+#
+#   puts user1.location.city
+
 
   #Hmm. It doesn't work. Oh bother.
   #How would we possibly fix this so I don't have to change the name of MY class but still be able to use this gem?
@@ -95,53 +142,62 @@
   #########     CONSTANTS    ########
   ###################################
 
-  #A Ruby constant is like a variable, except that its value is supposed to remain constant for the duration of the program. The Ruby interpreter does not actually enforce the constancy of constants, but it does issue a warning if a program changes the value of a constant
+  # A Ruby constant is like a variable, except that its value is supposed to remain constant for the duration of the program. The Ruby interpreter does not actually enforce the constancy of constants, but it does issue a warning if a program changes the value of a constant
 
   #### NOTES ###
 
   # Constants defined within a class or module may be accessed anywhere within the class or module.
 
-  # Outside the class or module, they may be accessed using the scope operator, :: prefixed by an expression that returns the appropriate class or module.
+  # Outside the class or module, they may be accessed using the scope operator, :: prefixed by an
+  # expression that returns the appropriate class or module.
 
   # Constants defined outside any class or module may be accessed as it is or by using the scope operator with no prefix.
   # Constants may not be defined in methods.
 
-  # Constants may be added to existing classes and modules from the outside by using the class or module name and the scope operator before the constant name.
+  # Constants may be added to existing classes and modules from the outside by using the class or module name
+  # and the scope operator before the constant name.
+
+      module Blackjack
+
+          MAX_SCORE = 21
+
+          module Player
 
 
+              class Player
+              MAX_PLAYERS = 2
+                def initialize
+                end
+              end
 
-      #   module Blackjack
-      #     MAX_SCORE = 21
-      #     module Player
-      #         MAX_PLAYERS = 2
-      #         class Player
-      #           def initialize
-      #           end
-      #         end
-      #     end
-      #     module Card
-      #         MAX_VALUE = 13
-      #         class Card
-      #           def get_max_value()
-      #               return MAX_VALUE
-      #           end
-      #         end
-      #     end
-      # end
-      #
-      # MAX_SCORE = 50
-      # MAX_PLAYERS = 4
-      # MAX_VALUE = 21
-      #
-      # puts "MAX_SCORE - #{MAX_SCORE}"
-      # puts "Blackjack::MAX_SCORE - #{Blackjack::MAX_SCORE}"
-      #
-      # puts
-      #
-      # puts "MAX_Players - #{MAX_PLAYERS}"
-      # puts "Blackjack::Player::MAX_Players - #{Blackjack::Player::MAX_PLAYERS}"
-      #
-      # puts
-      #
-      # puts "MAX_VALUE - #{MAX_VALUE}"
-      # puts "Blackjack::Card::get_max_value -  #{Blackjack::Card::Card.new().get_max_value()}"`
+          end
+
+          module Card
+
+              MAX_VALUE = 13
+              class Card
+                def get_max_value()
+                    return MAX_VALUE
+                end
+              end
+
+          end
+
+      end
+
+      MAX_SCORE = 50
+      MAX_PLAYERS = 4
+      MAX_VALUE = 21
+
+      puts "MAX_SCORE - #{MAX_SCORE}"
+      puts "Blackjack::MAX_SCORE - #{Blackjack::MAX_SCORE}"
+
+      puts
+
+      puts "MAX_Players - #{MAX_PLAYERS}"
+      puts "Blackjack::Player::Player::MAX_Players - #{Blackjack::Player::Player::MAX_PLAYERS}"
+
+      puts
+
+      puts "MAX_VALUE - #{MAX_VALUE}"
+      puts "Blackjack::Card::get_max_value -  #{Blackjack::Card::Card.new().get_max_value()}"
